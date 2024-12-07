bdd28adca6b9021a576f43ab4124d98f619b06fb
yite <yite@icloud.com>
2024-12-07 08:15:16 +0800

    update insert data

--- argparser/insert_kline_data.py
+++ argparser/insert_kline_data.py
@@ -15,32 +15,34 @@ def insert_kline_parser():
     parser = argparse.ArgumentParser(description="Insert an API key")
     parser.add_argument("--symbol", required=True, help="Symbol, e.g. BTCUSDT")
     parser.add_argument("--interval", required=True, help="Interval, e.g. 1m")
-    parser.add_argument("--start_date", required=True, help="YYYY-MM-DD (Inclusive)")
-    parser.add_argument("--end_date", required=True, help="YYYY-MM-DD (Inclusive)")
+    parser.add_argument("--start_ts", type=int, required=True, help="Start timestamp in milliseconds (Unix time)")
+    parser.add_argument("--end_ts", type=int, required=True, help="End timestamp in milliseconds (Unix time)")
     return parser
 
 if __name__ == "__main__":
     parser = insert_kline_parser()
     args = parser.parse_args()
     c = Client()
-    start_utc = datetime.strptime(args.start_date, "%Y-%m-%d").replace(tzinfo=timezone.utc)
-    end_utc = datetime.strptime(args.end_date, "%Y-%m-%d").replace(tzinfo=timezone.utc)
+    start_ts = args.start_ts
+    end_ts = args.end_ts
+    day_interval = 86400000  # One day in milliseconds
+    num_days = (end_ts - start_ts) // day_interval + 1  # Include the end date
+
     columns = [
-        "open_time", "open", "high", "low", "close", "base_asset_volume",
+        "open_time", "open_px", "high_px", "low_px", "close_px", "base_asset_volume",
         "close_time", "quote_asset_volume", "number_of_trades",
         "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume", "unused_field"
     ]
     # Calculate the number of days to iterate
-    num_days = (end_utc - start_utc).days + 1  # Include the end date
     engine = create_engine(db_url)
+    current_ts = start_ts
     # Use tqdm to add a progress bar
     for i in tqdm(range(num_days), desc="Processing dates"):
-        current_date = start_utc + timedelta(days=i)
-        start_ts = int(current_date.timestamp() * 1000)
-        end_ts = int((current_date + timedelta(days=1)).timestamp() * 1000) - 1
-        klines = c.get_historical_klines(args.symbol, args.interval, start_ts, end_ts)
+        next_ts = min(current_ts + day_interval, end_ts)
+        klines = c.get_historical_klines(args.symbol, args.interval, current_ts, next_ts - 1)
         df_kline = pd.DataFrame(klines, columns=columns)
         df_kline["symbol"] = args.symbol
         df_kline["interval"] = args.interval
         df_kline.drop(columns=["unused_field"], inplace=True)
         df_kline.to_sql("kline", con=engine, if_exists="append", index=False)
+        current_ts = next_ts
